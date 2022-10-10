8c96bb384bf3d0fe198c4271499fcd1f825a5f84
Sangio1990 <stefanosangiovanni90@gmail.com>
2022-10-10 14:08:01 +0200

    first API implemented

--- projects/001-calculator-with-apis/server.js
+++ projects/001-calculator-with-apis/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 // Running the server
 const app = express();
+app.use(bodyParser.json())
 app.listen(8000, () => console.log("Server running at 8000"));
 
 //Setting the homepage
@@ -11,11 +13,62 @@ app.get("", (req, res) => {
     res.end('List of possible operations:\n');
 });
 
+//Setting some generic functions
+function returnNumbers(req) {
+    var body = req.body;
+    var numbers = [];
+    for (let n in body.numbers){
+        numbers.push(parseFloat(body.numbers[n]));
+    }
+    return numbers;
+}
+
 //Setting the APIs
 app.get("/API/sum", (req, res) =>{
-    var num1 = parseInt(req.query.numbers[0])
-    var num2 = parseInt(req.query.numbers[2])
+    var total = 0;
+    var numbers = returnNumbers(req);
+    for (let n in numbers){
+        total += numbers[n];
+    }
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end(`The sum of ${num1} + ${num2} is ${num1+num2}`);
+    res.end(`The sum total is ${total}`);
+})
+
+app.get("/API/mul", (req, res) => {
+    var total = 1;
+    var numbers = returnNumbers(req)
+    for (let n in numbers){
+        total *= numbers[n];
+    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`The multiplication total is ${total}`);
+
+})
+
+app.get("/API/div", (req, res)=>{
+    var numbers = returnNumbers(req)
+    var total = numbers[0];
+    for (let n in numbers){
+        if (n != 0){
+            total /= numbers[n];
+        }
+    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`The division total is ${total}`);
+})
+
+app.get("/API/sub", (req, res) => {
+    var numbers = returnNumbers(req)
+    var total = numbers[0];
+    for (let n in numbers){
+        if (n != 0){
+            total -= numbers[n];
+        }
+    }
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(`The subtraction total is ${total}`);
 })
